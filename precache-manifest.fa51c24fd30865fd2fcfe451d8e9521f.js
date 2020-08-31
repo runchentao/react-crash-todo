69579ab34ad244655dfd2900efa944b4988bcb7b
runchentao <runchentao@gmail.com>
2020-08-31 16:19:13 -0700

    Updates

---
+++ precache-manifest.fa51c24fd30865fd2fcfe451d8e9521f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f171fc6c4cc32d27d5b390f3e205ad3",
+    "url": "/react-crash-todo/index.html"
+  },
+  {
+    "revision": "0911c9f8d705114f4cae",
+    "url": "/react-crash-todo/static/css/main.d12423a4.chunk.css"
+  },
+  {
+    "revision": "4d9562894780d617d15b",
+    "url": "/react-crash-todo/static/js/2.cc226504.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-crash-todo/static/js/2.cc226504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0911c9f8d705114f4cae",
+    "url": "/react-crash-todo/static/js/main.c64f7806.chunk.js"
+  },
+  {
+    "revision": "2d93ff17c6a2edae4235",
+    "url": "/react-crash-todo/static/js/runtime-main.7d18b92c.js"
+  }
+]);
